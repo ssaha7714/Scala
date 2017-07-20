ae63bf9b9d3a29f260e3cf69a4d304d64a7055a2
ssaha7714 <swarup.ju@gmail.com>
2017-07-21 00:05:43 +0530

    Initial commit

---
+++ src/main/scala/OddEven.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by ssaha7714 on 20/07/17.
+  */
+object OddEven {
+  def main(args: Array[String]): Unit = {
+    val (odd,even) = (1 to 100) partition(_%2!=0)
+    println(odd)
+    println(even)
+
+  }
+}
---
+++ src/main/scala/VarVsVal.scala
@@ -0,0 +1,36 @@
+/**
+  * Created by ssaha7714 on 20/07/17.
+  */
+object VarVsVal {
+
+  def main(args: Array[String]): Unit = {
+    // Crete an Array
+    var arr = Array(1,2,3) // Val is Immutable
+    // Print the Array
+    println("Printing Array Elements..")
+    arr.foreach(x=>println(x))
+
+    /**
+      * The above code snippet is same as below
+      */
+    // Print the Array - Approach 2
+    println("Printing Array Elements..")
+    arr.foreach(println)
+
+
+    arr.update(0,5)
+    println("Printing Array Elements.. After Update")
+    arr.foreach(println)
+
+    println("Adding new elements to the Array")
+
+    arr = arr :+ 23 // This line is gining error beacuse we have used val not var
+
+    println("Printing Array Elements.. After Adding new element")
+    arr.foreach(println)
+
+    arr.foreach(println)
+
+
+  }
+}
